4cab197dbdd83b3508fe8b78b5c8345da570e3aa
maxstbn <mastbn@gmail.com>
2011-07-06 21:26:57 +0400

    сделал бронирование столиков

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class BookingMailer < ActionMailer::Base
   default_url_options[:host] = "localhost:3000"
 
@@ -5,7 +6,7 @@ class BookingMailer < ActionMailer::Base
 
   def booking_confirmation(booking)
     @booking = booking
-    mail(:to => "maxstbn@gmail.com", :subject => "Registered")
+    mail(:to => "maxstbn@gmail.com", :subject => "Новая заявка на бронирование стола")
   end
 
 end
